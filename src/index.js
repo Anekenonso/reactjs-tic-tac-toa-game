ca8cd565a366936bd20c58f6f730b888de58cf6c
anekenonso <okenwanonsokenneth@gmail.com>
2020-06-11 21:31:35 +0100

    updating the game

--- src/index.js
+++ src/index.js
@@ -4,18 +4,44 @@ import ReactDom from 'react-dom';
 import './index.css';
 
 class Square extends React.Component {
+
     render() {
         return(
-            <button className="square">
-              {}
+            <button 
+            className="square"
+            onClick={() => 
+                this.props.onClick()}
+            >
+              {this.props.value}
             </button>
         );
     }
 }
 
 class Board extends React.Component {
+
+   constructor(props){
+       super(props);
+       this.state = {
+           squares: Array(9).fill(null),
+       };
+   }
+
+   handleClick(i) {
+
+       const squares = this.state.squares.slice();
+       squares[i] = 'x';
+       this.setState({squares: squares});
+   }
+
     renderSquare(i) {
-        return <Square />
+        return (
+        
+        <Square value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+        />
+
+        ); 
     }
 
     render() {
